0d9357ee4deea7762dca91b8d872113b37659ca8
mujeeb-netizen <mujeebahmed97@gmail.com>
2023-06-07 01:54:14 +0200

    ex2 done

---
+++ exercises/exercise2.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def getDataFromLink(link):
+    dataFrame = pd.read_csv(link, delimiter=";")
+    return dataFrame
+    
+
+
+def createSQLiteFile(df):
+    df.to_sql("trainstops", 'sqlite:///trainstops.sqlite',if_exists='replace', index=False)
+    
+    
+
+    
+def cleanData(data):
+    data.drop(columns=["Status"], inplace=True)
+    data = data[data["Verkehr"].isin(["FV","RV","nur DPN"])]
+    data['Laenge'] = data['Laenge'].str.replace(',','.')
+    data['Breite'] = data['Breite'].str.replace(',','.')
+    data["Laenge"] = pd.to_numeric(data["Laenge"])
+    data["Breite"] = pd.to_numeric(data["Breite"])
+    data= data[~(data["Laenge"] < 90) & data["Laenge"] > -90]
+    data= data[~(data["Breite"] < 90) & data["Breite"] > -90]
+    data = data.dropna()
+    data['IFOPT'] = df['IFOPT'].str.match(r'^[a-zA-Z]{2}:[0-9]*:[0-9]+(:[0-9]+)?$', case=False)
+    return data
+    
+
+link = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
+df = getDataFromLink(link)
+df = cleanData(df)
+createSQLiteFile(df)
