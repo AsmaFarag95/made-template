49ddd46ea5801e905bce630efa746d064085864d
mujeeb-netizen <mujeebahmed97@gmail.com>
2023-07-05 11:05:55 +0200

    exercise 4

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -14,14 +14,10 @@ types = {
 
 def getZipFileFromLink(link):
     csv_file = "data.csv"
-    urllib.request.urlretrieve(link, "md.zip")
-    with zipfile.ZipFile("md.zip", 'r') as rref:
-        rref.extract(csv_file)
-    return csv_file
+
  
 def getDataFromFile(file_):
     df = pd.read_csv(file_, delimiter=';', index_col=False, usecols=["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"])
-    df.columns =["Geraet", "Hersteller", "Model", "Monat", "Temperatur", "Batterietemperatur", "Geraet aktiv"]
     return df
     
 
@@ -38,20 +34,13 @@ def changeDataType(df,types):
 def validateAndTransformData(data):
     
     data["Temperatur"] = (data["Temperatur"].astype(str).str.replace(',', '.').astype(float)*9/5)+32
-    data["Batterietemperatur"] = (data["Batterietemperatur"].astype(str).str.replace(',', '.').astype(float)*9/5)+32
-    data["Monat"] = data["Monat"].astype(int)
-    data["Geraet"] = data["Geraet"].astype(int)
-    data = data[(data['Geraet'] > 0) & (data['Monat'].between(1, 12))& (data["Temperatur"].between(-459.67, 212))& (data["Batterietemperatur"].between(-459.67, 212)) & (data["Geraet aktiv"].isin(["Ja", "Nein"]))]
+  
     return data
     
 def init():
     link = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
     file_ = getZipFileFromLink(link)
-    df = getDataFromFile(file_)
-    df = validateAndTransformData(df)
-    df = changeDataType(df,types)
-    print(df)
-    createSQLiteFile(df)
+
    
 
     
