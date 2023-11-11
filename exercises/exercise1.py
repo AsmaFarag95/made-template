a6222ab2b703f6581946ea0ee105b158a7366c5c
AsmaFarag95 <134290864+AsmaFarag95@users.noreply.github.com>
2023-11-12 00:06:50 +0100

    Update with for loop exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,28 +1,18 @@
 import pandas as pd
 from sqlalchemy import create_engine, Integer, Text, Float
 
-# Source URL
-source_url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
-
 # Read CSV data into a DataFrame
-df = pd.read_csv(source_url, delimiter=";")
+df = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv", delimiter=";")
 
-# Define SQLite data types for each column
-sqlite_data_types = {
-    "column_1": Integer(),
-    "column_2": Text(),
-    "column_3": Text(),
-    "column_4": Text(),
-    "column_5": Text(),
-    "column_6": Text(),
-    "column_7": Float(),
-    "column_8": Float(),
-    "column_9": Integer(),
-    "column_10": Float(),
-    "column_11": Text(),
-    "column_12": Text(),
-    "geo_punkt": Text(),
-}
+# Define SQLite data types for each column using a loop
+sqlite_data_types = {}
+for column in df.columns:
+    if df[column].dtype == 'int64':
+        sqlite_data_types[column] = Integer()
+    elif df[column].dtype == 'float64':
+        sqlite_data_types[column] = Float()
+    else:
+        sqlite_data_types[column] = Text()
 
 # Create SQLite database engine
 engine = create_engine("sqlite:///airports.sqlite")
