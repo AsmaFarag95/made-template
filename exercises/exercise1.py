6acc6d2fb6d1319914d29ffd5cbd6457bfc6a44d
AsmaFarag95 <134290864+AsmaFarag95@users.noreply.github.com>
2023-11-11 23:57:58 +0100

    Update exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,34 +1,34 @@
 import pandas as pd
- 
-# Source URL
-source_url =   "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
+from sqlalchemy import create_engine, Integer, Text, Float
 
+# Source URL
+source_url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
 
 # Read CSV data into a DataFrame
-df = pd.read_csv(source_url,   delimiter=";")
-
+df = pd.read_csv(source_url, delimiter=";")
 
 # Define SQLite data types for each column
 sqlite_data_types = {
-    "column_1": "int",
-    "column_2": "str",
-    "column_3": "str",
-    "column_4": "str",
-    "column_5": "str",
-    "column_6": "str",
-    "column_7": "float",
-    "column_8": "float",
-    "column_9": "INTEGER",
-    "column_10": "int",
-    "column_11": "str",
-    "column_12": "str",
-    "geo_punkt": "str",
+    "column_1": Integer(),
+    "column_2": Text(),
+    "column_3": Text(),
+    "column_4": Text(),
+    "column_5": Text(),
+    "column_6": Text(),
+    "column_7": Float(),
+    "column_8": Float(),
+    "column_9": Integer(),
+    "column_10": Float(),
+    "column_11": Text(),
+    "column_12": Text(),
+    "geo_punkt": Text(),
 }
 
+# Create SQLite database engine
+engine = create_engine("sqlite:///airports.sqlite")
 
 # Create the SQLite table
-df.to_sql("airportspy", "sqlite:///airportspy.sqlite" , if_exists="replace", index=False , dtype = sqlite_data_types)
-print("df", df)
+df.to_sql("airports", con=engine, if_exists="replace", index=False, dtype=sqlite_data_types)
 
 # Confirm the successful creation of the table
 print("Table 'airports' created successfully.")
