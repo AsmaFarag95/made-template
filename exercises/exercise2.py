60067c991a4bcb49e370139db79946ba47b04bc0
AsmaFarag95 <134290864+AsmaFarag95@users.noreply.github.com>
2023-11-26 15:57:57 +0100

    Update exercise2.py

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -13,42 +13,41 @@ types = {
 
 
 def getDataFromLink(link):
-    data_frame = pd.read_csv(link, delimiter=";")
-    return data_frame
+    dataFrame = pd.read_csv(link, delimiter=";")
+    return dataFrame
+    
+
 
 def createSQLiteFile(df):
     df.to_sql("trainstops", 'sqlite:///trainstops.sqlite',if_exists='replace', index=False)
     
     
 def changeDataType(df,types):
-    df = data_frame.astype(types)
+    df = df.astype(types)
     return df
     
     
     
-def cleanData(df):
-    df.drop(columns=["Status"], inplace=True)
-    df = data_frame[df["Verkehr"].isin(["FV","RV","nur DPN"])]
+def cleanData(data):
+    data.drop(columns=["Status"], inplace=True)
+    data = data[data["Verkehr"].isin(["FV","RV","nur DPN"])]
+    data_frame.loc[:, 'Laenge'] = data_frame['Laenge'].str.replace(',', '.')
+    data_frame.loc[:, 'Breite'] = data_frame['Breite'].str.replace(',', '.')
 
-    df['Laenge'] = df['Laenge'].str.replace(',','.')
-    df['Breite'] = df['Breite'].str.replace(',','.')
 
-
-    df = df.dropna()
-    df = changeDataType(df, types)
-
-    df = df[~((df["Laenge"] <= 90) & (df["Laenge"] >= -90))]
-    df = df[~((df["Breite"] <= 90) & (df["Breite"] >= -90))]
-
-    df = df[df['IFOPT'].str.contains(r'^[a-zA-Z]{2}:[0-9]*:[0-9]+(:[0-9]+)?$')]
-    df.IFOPT = df.IFOPT.astype(str)
-    return df
-
+    data = data.dropna()
+    data = changeDataType(data,types)
+    data= data[~(data["Laenge"] <= 90) & data["Laenge"] >= -90]
+    data= data[~(data["Breite"] <= 90) & data["Breite"] >= -90]
+    
+    print(type(data['IFOPT']))
+    data = data[data['IFOPT'].str.contains(r'^[a-zA-Z]{2}:[0-9]*:[0-9]+(:[0-9]+)?$')]
+    data.IFOPT = data.IFOPT.astype(str)
+    return data
     
 def init():
-    csv_link = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
- #call the methods:
-    data_frame = getDataFromLink(csv_link)
+    link = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
+    data_frame = getDataFromLink(link)
     data_frame = cleanData(data_frame)
     createSQLiteFile(data_frame)
 
@@ -56,5 +55,3 @@ def init():
     
 if __name__ == "__main__":
     init()  
-
-  
