80cc579f79f9fd681ac48af4a5dc12856ec8b4f1
Asma <asmafaraj95@gmail.com>
2023-11-30 05:49:46 +0100

    update the project

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,36 +1,5 @@
 import pandas as pd
-<<<<<<< HEAD
-from sqlalchemy import create_engine, Integer, Text, Float
  
-# Read CSV data into a DataFrame
-df = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv", delimiter=";")
-
-# Define SQLite data types for each column
-sqlite_data_types = {
-    "column_1": Integer(),
-    "column_2": Text(),
-    "column_3": Text(),
-    "column_4": Text(),
-    "column_5": Text(),
-    "column_6": Text(),
-    "column_7": Float(),
-    "column_8": Float(),
-    "column_9": Integer(),
-    "column_10": Float(),
-    "column_11": Text(),
-    "column_12": Text(),
-    "geo_punkt": Text(),
-}
-
-# Create SQLite database engine
-engine = create_engine("sqlite:///PYairports.sqlite" , echo=True)
-
-# Create the SQLite table
-df.to_sql("PYairports", con=engine, if_exists="replace", index=False, dtype=sqlite_data_types)
-
-# Confirm the successful creation of the table
-print("Table 'airports' created successfully.")
-=======
 from sqlalchemy import create_engine, Integer, Text, Float #create_engine is a function from sqlalchemy used to create a connection to the SQLite database.
 
 # Read CSV data into a DataFrame,  This DataFrame is  a dataset that holds the tabular data from the CSV file.
@@ -54,4 +23,4 @@ engine = create_engine("sqlite:///airports.sqlite")
 df.to_sql("airports", con=engine, if_exists="replace", index=False, dtype=sqlite_data_types)
 
  
->>>>>>> origin/main
+ 
--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,87 +1,4 @@
-<<<<<<< HEAD
-'''
-Exercise 2
-Build an automated data pipeline for the following source:
-direct link to CSV: https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV
 
-Goal
-
-Write data into a SQLite database called “trainstops.sqlite”, in the table “trainstops”
-
-First, drop the "Status" column
-
-Then, drop all rows with invalid values:
-Valid "Verkehr" values are "FV", "RV", "nur DPN"
-Valid "Laenge", "Breite" values are geographic coordinate system values between and including -90 and 90
-
-Valid "IFOPT" values follow this pattern:
-<exactly two characters>:<any amount of numbers>:<any amount of numbers><optionally another colon followed by any amount of numbers>
-This is not the official IFOPT standard, please follow our guidelines and not the official standard
-Empty cells are considered invalid
-
-Use fitting SQLite types (e.g., BIGINT, TEXT or FLOAT) for all columns
-
-Allowed libraries: https://github.com/jvalue/made-exercise-feedback/blob/main/requirements.txt
- 
-Submit as  /exercises/exercise2.jv or  /exercises/exercise2.py depending on your language
- 
-'''
-import pandas as pd
-from sqlalchemy import create_engine
-
-# Define the CSV link
-csv_link = "/Users/asmafaraj/Downloads/D_Bahnhof_2020_alle.csv"
-
-# Define SQLite database and table names
-SQLdb_name = "trainstops.sqlite"
-table_name = "trainstops"
-
-# Read the CSV into a pandas DataFrame
-df = pd.read_csv(csv_link, delimiter=";")
-
-# Drop the "Status" column
-df = df.drop("Status", axis=1) #last clm # (axis=1 Means we want to operate on clms).
-
-# Define a function to check and clean values
-def clean_values(value):
-    if pd.isnull(value):
-        return False
-    return True
-
-# Apply the clean_values function to each relevant column
-columns_to_clean = ["Verkehr", "Laenge", "Breite", "IFOPT"]
-for column in columns_to_clean:
-    df = df[df[column].apply(clean_values)]
-
-# Filter rows based on "Verkehr" values
-valid_verkehr_values = ["FV", "RV", "nur DPN"]
-df = df[df["Verkehr"].isin(valid_verkehr_values)]
-
-# Filter rows based on "Laenge" and "Breite" values
-df = df[(df["Laenge"] >= -90) & (df["Laenge"] <= 90) & (df["Breite"] >= -90) & (df["Breite"] <= 90)]
-
-# Filter rows based on the IFOPT pattern
-#IFOPT, or Identifikationscode für ÖPNV-Teilnehmer (Identification of Fixed Objects in Public Transport), 
-ifo_pattern = r'^[a-zA-Z]{2}:\d+:\d+(:\d+)?$'
-df = df[df["IFOPT"].str.match(ifo_pattern)]
-
-# Define SQLite types for each column
-sqlite_types = {
-    "EVA_NR": "INT",
-    "DS100": "TEXT",
-    "IFOPT": "TEXT",
-    "NAME": "TEXT",
-    "Verkehr": "TEXT",
-    "Laenge": "FLOAT",
-    "Breite": "FLOAT",
-    "Betreiber_Name": "TEXT",
-    "Betreiber_Nr": "TEXT",
-}
-
-# Create an SQLite engine and write the DataFrame to the database
-engine = create_engine(f"sqlite:///{SQLdb_name}")
-df.to_sql(table_name, engine, index=False, if_exists="replace", dtype=sqlite_types)
-=======
 import pandas as pd
 types = {
             "EVA_NR": int,
@@ -139,4 +56,4 @@ def init():
     
 if __name__ == "__main__":
     init()  
->>>>>>> origin/main
+ 
