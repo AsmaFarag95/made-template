50d0ad4d0fbdc9965373cf9ec5697f9d18bdc1d7
Asma <asmafaraj95@gmail.com>
2024-01-12 04:09:11 +0100

    ex4

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -2,49 +2,65 @@ import pandas as pd
 import zipfile
 import urllib.request
 
-types = {
-    'Geraet': int,
-    'Hersteller': str,
-    'Model': str,
-    'Monat': int,
-    'Temperatur': float,
-    'Batterietemperatur': float,
-    'Geraet aktiv': str
-}
 
-url = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
+data_types = {
+        'Geraet': int,
+        'Hersteller': str,
+        'Model': str,
+        'Monat': int,
+        'Temperatur': float,
+        'Batterietemperatur': float,
+        'Geraet aktiv': str
+        }
 
-csv_file = "data.csv"
-urllib.request.urlretrieve(url, "mowesta-dataset.zip")
-with zipfile.ZipFile("mowesta-dataset.zip", 'r') as rref:
-    rref.extract(csv_file)
 
-file_cv = csv_file
+def get_zip_file_from_link(url):
+    csv_file = "data.csv"
+    urllib.request.urlretrieve(url, "mowesta_dataset.zip")
+    with zipfile.ZipFile("mowesta_dataset.zip", 'r') as rref:
+        rref.extract(csv_file)
+    return csv_file
+ 
+def get_data_from_file(file_cv):
+    df = pd.read_csv(file_cv, delimiter=';', index_col=False, usecols=["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"])
+    df.columns =["Geraet", "Hersteller", "Model", "Monat", "Temperatur", "Batterietemperatur", "Geraet aktiv"]
+    return df
+    
 
-# Only use the columns "Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"
-# Rename "Temperatur in °C (DWD)" to "Temperatur"
-# Rename "Batterietemperatur in °C" to "Batterietemperatur"
-df = pd.read_csv(file_cv, delimiter=';', index_col=False, usecols=["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"])
-df.columns = ["Geraet", "Hersteller", "Model", "Monat", "Temperatur", "Batterietemperatur", "Geraet aktiv"]
-# Discard all columns to the right of "Geraet aktiv"
-df = df.loc[:, :"Geraet aktiv"]
+    
+def fitting_data_types(df,types):
+    df = df.astype(types)
+    return df
+    
+    
 
-# Transform temperatures in Celsius to Fahrenheit
-df["Temperatur"] = (df["Temperatur"].str.replace(',', '.').astype(float) * 9/5) + 32
-df["Batterietemperatur"] = (df["Batterietemperatur"].str.replace(',', '.').astype(float) * 9/5) + 32
 
-# Validate and filter data
-df["Monat"] = df["Monat"].astype(int).between(1, 12 , inclusive='both')
-df["Geraet"] = df["Geraet"].astype(int)
-df = df[(df['Geraet'] > 0)]
-df["Temperatur"] = df["Temperatur"].between(-459.67, 212)
-df["Batterietemperatur"] = df["Batterietemperatur"].between(-459.67, 212)
-df["Geraet aktiv"] = df["Geraet aktiv"].isin(["Ja", "Nein"])
+      
+def validations_as(data):
+    
+    data["Temperatur"] = (data["Temperatur"].astype(str).str.replace(',', '.').astype(float)*9/5)+32
+    data["Batterietemperatur"] = (data["Batterietemperatur"].astype(str).str.replace(',', '.').astype(float)*9/5)+32
+    data["Monat"] = data["Monat"].astype(int)
+    data["Geraet"] = data["Geraet"].astype(int)
+    data = data[(data['Geraet'] > 0) & (data['Monat'].between(1, 12))& (data["Temperatur"].between(-459.67, 212))& (data["Batterietemperatur"].between(-459.67, 212)) & (data["Geraet aktiv"].isin(["Ja", "Nein"]))]
+    return data
 
-# Convert types
-df = df.astype(types)
 
-# Write data into a SQLite database called “temperatures.sqlite”, in the table “temperatures”
-df.to_sql("temperatures", 'sqlite:///temperatures.sqlite', if_exists='replace', index=False)
+def create_SQLite_file(df):
+    df.to_sql("temperatures", 'sqlite:///temperatures.sqlite',if_exists='replace', index=False )
+    
+    
+def init():
+    url = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
+    file_cv = get_zip_file_from_link(url)
+    df = get_data_from_file(file_cv)
+    df = validations_as(df)
+    df = fitting_data_types(df,data_types)
+    print(df)
+    create_SQLite_file(df)
+   
 
-print(df)
+    
+    
+if __name__ == "__main__":
+    init()  
